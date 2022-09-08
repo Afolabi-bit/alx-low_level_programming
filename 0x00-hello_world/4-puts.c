23322fd147d74fa170c6696dac411f71c2005b3f
afolabi <maverickoluwatomisin@gmail.com>
2022-09-08 13:29:13 +0100

    First actual C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
-
 /**
  * Prints a string
  *
  */
 
-int main()
+int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
+	/**
+	 * This function prints a string
+	 *
+	 * Return: 0 to terminate execution
+	 */
+	
+	char str[] = "/"Programming is like building a multilingual puzzle";
 
 	puts(str);
 
-	return 0;
+	return (0);
 }
