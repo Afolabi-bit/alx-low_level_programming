69a58b1b57eae01b49ca63fc790a9805e521f27a
afolabi <maverickoluwatomisin@gmail.com>
2022-09-08 14:32:23 +0100

    Printing sizes of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+/*
+ *File: 6-size.c
+ *Author: Afolabi
+ */
+
+#include <stdio.h>
+
+/*
+ *main - Prints the size of various types on the computer it is compiled and run on.
+ *Return: Always returns 0.
+ */
+int main(void)
+{
+
+	printf("sizeof(int)");
+	printf("sizeof(char)");
+	printf("sizeof(double)");
+	printf("sizeof(float)");
+	
+	return (0);
+}
