fd194346d6552744c197c3276274a37de83ad385
afolabi <maverickoluwatomisin@gmail.com>
2022-09-09 16:07:38 +0100

    Combination of all letters formed from 0 - 9

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,18 @@ int main(void)
 	{
 		for (m = n + 1; m < 10; m++)
 		{
-			putchar(n + '0');
-			putchar(m + '0');
+			//putchar(n + '0');
+			//putchar(m + '0');
 			if (n != 8 && m != 9)
 			{
+				putchar(n + '0');
+				putchar(m + '0');
 				putchar(',');
 				putchar(' ');
+			} else 
+			{
+				putchar(n + '0');
+				putchar(m + '0');
 			}
 		}
 
