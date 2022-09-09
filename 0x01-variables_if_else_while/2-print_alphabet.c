77265d7f9370c2c2d15d6e5ae6b82be3c287e20c
afolabi <maverickoluwatomisin@gmail.com>
2022-09-09 13:17:30 +0100

    Prints a - z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ * Return: Zero
+ */
+int main(void)
+{
+	char c;
+
+	for(c = "a"; c <= "z"; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+
+}
