768735907088da374e3cd7918b38269282bfa3f0
afolabi <maverickoluwatomisin@gmail.com>
2022-09-09 15:45:45 +0100

    Combination of all letters formed from 0 - 9

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - All possible combinations of two digits
+ * Return: Zero
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = 1; m < 10; m++)
+		{
+			putchar(m + '0');
+			putchar(m + '0');
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
