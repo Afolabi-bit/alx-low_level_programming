2b612ca1009952e100bf0c47500ee31956dc9786
afolabi <maverickoluwatomisin@gmail.com>
2022-09-13 19:05:24 +0100

    times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints times table
+ * Description: prints times table from 1 - 15
+ * @n: integer val
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	if ((n > 15) || (n < 0))
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				int res;
+
+				res = i * j;
+				if (j == 0)
+				{
+					--putchar('0' + res);
+				} else if (res >= 10)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				} else if (res >= 100)
+				{
+					_putchar(' ');
+					_putchar((res / 100) + '0');
+					_putchar(((res / 10) % 10) + '0');
+					_putchar((res % 10) + '0');
+				} else
+				{
+					_putchar(' ');
+					_putchar(res + '0');
+				}
+				if (j != n)
+				{
+					_putchar(',');
+				}
+			}
+			_putchar('\n');
+		}
+}
