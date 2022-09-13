3d6df227b844e955f3e15d45afb81c775d952b9e
afolabi <maverickoluwatomisin@gmail.com>
2022-09-13 17:11:10 +0100

    Minutes of day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,22 +7,17 @@
  */
 void jack_bauer(void)
 {
-	int h, m, a, b, c, d;
+	int h, m;
 
 	for (h = 0; h < 25; h++)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			a = h / 10;
-			b = h % 10;
-			c = m / 10;
-			d = m % 10;
-			_putchar(a + '0');
-			_putchar(b + '0');
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
 			_putchar(':');
-			_putchar(c + '0');
-			_putchar(d + '0');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
 			_putchar('\n');
-		}
 	}
 }
