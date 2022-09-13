8d80ca9df0642f67bcdca3d7a9bb5726c82c87ff
afolabi <maverickoluwatomisin@gmail.com>
2022-09-13 19:29:55 +0100

    multiples

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 	int i;
-	int sum = 0;
+	int total = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
 		{
-			sum += i;
+			total += i;
 		}}
 	}
-	printf("%d\n", sum);
+	printf("%d\n", toatl);
 
 	return (0);
 }
