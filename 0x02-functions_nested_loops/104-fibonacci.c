334b53b55a23a8a6aea070fb94b85f346ad36a72
afolabi <maverickoluwatomisin@gmail.com>
2022-09-13 22:02:09 +0100

    Fib 3

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 98 fibonacci nums
+ * Description: Fibonacci sequence
+ * Return: Zero
+ */
+int main(void)
+{
+	unsigned long num, seq, sum;
+	int i;
+
+	num = 1;
+	seq = 2;
+	sum = 0;
+
+	for (i = 0; i <= 98; i++)
+	{
+		if (i != 98)
+		{
+			printf("%d, ", num);
+			sum = num + sum;
+			num = seq;
+			seq = sum;
+		}
+		else
+		{
+			printf("%ld\n", num);
+		}
+	}
+	return (0);
+}
