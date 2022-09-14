7b5f83f2d0152d9b9a167fa0ad9b7f62ef6c4ba2
afolabi <maverickoluwatomisin@gmail.com>
2022-09-14 13:36:13 +0100

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,13 @@
 #include "main.h"
-
 /**
  * main - tests function that prints if integer is positive or negative
  * Description: Checks sign of numbers
  * Return: 0
  */
-
 int main(void)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
