be93c402a58617fb3260d1b733d19369f2c7e766
afolabi <maverickoluwatomisin@gmail.com>
2022-09-15 20:29:52 +0100

    nums

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * Description: Prints int
+ * @n: Num
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n >= 1000)
+	{
+		_putchar('0' + n / 1000);
+		_putchar('0' + (n % 1000) / 100);
+		_putchar('0' + (n % 100) / 10);
+		_putchar('0' + (n % 100) % 10);
+	}
+	else if (n >= 100)
+	{
+		_putchar('0' + n / 100);
+		_putchar('0' + (n % 100) / 10);
+		_putchar('0' + (n % 100) % 10);
+	}
+	else if (n >= 10)
+	{
+		_putchar('0' + n / 10);
+		_putchar('0' + n % 10);
+	}
+	else
+	{
+		_putchar('0' + n);
+	}
+}
