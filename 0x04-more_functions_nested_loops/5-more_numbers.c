915b579b0facfa1f1ddfcfcd91466ec018cc2ef5
afolabi <maverickoluwatomisin@gmail.com>
2022-09-15 14:10:36 +0100

    Prints ints

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,20 @@
  */
 void more_numbers(void)
 {
-	int i = 0, j = 0;
+	int i = 0, j;
 
 	for (; i < 10; i++)
 	{
-		for (; j < 15; j++)
+		j = 0;
+		while (j <= 14)
 		{
 			if (j >= 10)
 			{
 				_putchar(j / 10 + '0');
 			}
-			_putchar(j % 10 + '0');
+				_putchar(j % 10 + '0');
+			j++;
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
