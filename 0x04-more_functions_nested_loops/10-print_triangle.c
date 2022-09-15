6237757c2cd5a7aa13610cf7768eb259385fbe26
afolabi <maverickoluwatomisin@gmail.com>
2022-09-15 16:05:25 +0100

    Print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints triangle
+ * Description: Prints Triangle
+ * @size: Param
+ * Return: zero
+ */
+void print_triangle(int size)
+{
+	int l, b, ws;
+
+	l = 0;
+	while (l < size)
+	{
+		b = 0;
+		while (b < size)
+		{
+			ws = 0;
+			while (ws == b)
+			{
+				_putchar(' ');
+				ws--;
+			}
+			_putchar('#');
+			b++;
+		}
+		_putchar('\n');
+	}
+}
