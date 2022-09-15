08b3323befc1d8ddd426d2e32de6e5c662a7f859
afolabi <maverickoluwatomisin@gmail.com>
2022-09-15 22:21:31 +0100

    nums

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,26 +8,26 @@
  */
 void print_number(int n)
 {
-	unsigned int a, b, c;
+	unsigned int x, y, count;
 
 	if (n < 0)
 	{
-		a = -n;
-		_putchar('-');
+		_putchar(45);
+		x = -n;
 	}
 	else
 	{
-		a = n;
+		x = n;
 	}
-	b = a;
-	c = 1;
-	while (b > 9)
-	{
-		b /= 10;
-		count *= 10;
-	}
-	for (; c >= 1; c /= 10)
-	{
-		_putchar(((a / c) % 10) + '0');
-	}
+		y = x;
+		count = 1;
+		while (y > 9)
+		{
+			y /= 10;
+			count *= 10;
+		}
+		for (; count >= 1; count /= 10)
+		{
+			_putchar(((x / count) % 10) + 48);
+		}
 }
