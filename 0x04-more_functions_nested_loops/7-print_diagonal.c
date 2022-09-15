75f81ba0fe14a9dd800616f604324e59757af3d3
afolabi <maverickoluwatomisin@gmail.com>
2022-09-15 14:30:34 +0100

    Prints diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,7 @@
  */
 void print_diagonal(int n)
 {
-	if (n < 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
@@ -18,7 +18,7 @@ void print_diagonal(int n)
 
 		while (i < n)
 		{
-			_putchar('\');
+			_putchar('\\');
 			i++;
 		}
 		_putchar('\n');
