27467cfe002f4dc9217fc159cc216048f2f90b76
afolabi <maverickoluwatomisin@gmail.com>
2022-09-19 15:55:47 +0100

    Print str in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: String
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = 0, l;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	l = i;
+	i = 0;
+	j = l - 1;
+
+	while (i < j)
+	{
+		m = s[i];
+		s[i] = s[j];
+		i++;
+		j--;
+	}
+}
