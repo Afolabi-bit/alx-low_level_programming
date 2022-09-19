6d7fcf7a8fd2150819024ea01551008cb73edf5f
afolabi <maverickoluwatomisin@gmail.com>
2022-09-19 16:38:31 +0100

    Print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,16 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
 		printf("%d", a[i]);
 		if (i < (n - 1))
 		{
 			printf(", ");
 		}
+		i++;
 	}
-	_putchar('\n');
+	printf("\n");
 }
