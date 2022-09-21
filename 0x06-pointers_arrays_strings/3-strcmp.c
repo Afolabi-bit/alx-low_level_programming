e8c347008dd27f838ab53120d88547774db5d286
afolabi <maverickoluwatomisin@gmail.com>
2022-09-21 18:15:46 +0100

    String compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,12 +8,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int x = 0; /*i = 0*/
+	int x = 0, i = 0;
 
-	/*while ((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
+	while ((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
 	{
 		i++;
-	}*/
+	}
 	if (s1 != s2)
 	{
 		x = *s1 - *s2;
