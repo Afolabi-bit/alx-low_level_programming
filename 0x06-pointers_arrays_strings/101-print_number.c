dda75b84330d2fb96c3a21208665f9ee47d8457b
afolabi <maverickoluwatomisin@gmail.com>
2022-09-23 11:54:14 +0100

    Print integers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,26 +7,26 @@
  */
 void print_number(int n)
 {
-	unsigned int a, b, c;
+		unsigned int x, y, count;
 
-	if (n < 0)
-	{
-		_putchar(45);
-		a = -n;
-	}
-	else
-	{
-		a = n;
-	}
-	b = a;
-	c = 1;
-	while (b > 9)
-	{
-		b = b / 10;
-		c = c * 10;
-	}
-	for (; c >= 10; c /= 10)
-	{
-		_putchar(((a / c) % 10) + 48);
-	}
+			if (n < 0)
+					{
+								_putchar(45);
+										x = -n;
+											}
+				else
+						{
+									x = n;
+										}
+						y = x;
+								count = 1;
+										while (y > 9)
+													{
+																	y /= 10;
+																				count *= 10;
+																						}
+												for (; count >= 1; count /= 10)
+															{
+																			_putchar(((x / count) % 10) + 48);
+																					}
 }
