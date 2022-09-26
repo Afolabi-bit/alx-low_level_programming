2b13db07bb86e54b7f7e05d3f5f30c032ca932a4
afolabi <maverickoluwatomisin@gmail.com>
2022-09-26 14:47:17 +0100

    print chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints chessboard
+ * @a: Dimension of chessboard
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, e;
+
+	while (*a)
+	{
+		i = 0;
+		while ((*a)[i] < (*a)[8])
+		{
+			e = (*a)[i];
+			_putchar(e);
+			i++;
+		}
+		a++;
+	}
+}
