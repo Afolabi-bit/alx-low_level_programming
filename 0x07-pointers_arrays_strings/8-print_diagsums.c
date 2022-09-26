75bc122476c7a224a6cfa94ed893e3b89c6fac72
afolabi <maverickoluwatomisin@gmail.com>
2022-09-26 16:25:42 +0100

    Prints sum of diagonals in a sq matrix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - prints the sum of the two diagonals of a sq matrix
@@ -8,23 +9,15 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, k, sum1 = 0, sum2 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
 	i = 0;
 	while (i < size)
 	{
-		j = 0;
-		while (j < 5)
-		{
-			sum1 += a[i][j];
-			j++;
-		}
-		k = size;
-		while (k >= 0)
-		{
-			sum2 += a[i][k];
-			k--;
-		}
-		printf("%d, %d", sum1, sum2);
+		sum1 += *(a + (size * i) + i);
+		sum2 += *(a + (size * i + size -1 - i));
+		i++;
 	}
+
+		printf("%d, %d\n", sum1, sum2);
 }
