0713666d500467f5d121903c75a97a111861bef5
afolabi <maverickoluwatomisin@gmail.com>
2022-09-26 11:51:43 +0100

    memcpy  and strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a charcter in a string
+ * @s: Pointer to a string
+ * @c: character
+ * Return: pointer to address of c
+ */
+char *_strchr(char *s, char c)
+{
+	int x = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			x = s;
+			break;
+		}
+		s++;
+	}
+
+	return (x);
+}
