81441bd62ccfa82cc19892dcef5f8ed7d8c544f7
afolabi <maverickoluwatomisin@gmail.com>
2022-09-26 14:31:43 +0100

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,18 +8,21 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int i;
+
 	if (*needle == 0)
 		return (haystack);
 
-	while (*haystack != '\0')
+	while (*haystack)
 	{
-		while (*needle != '\0')
+		i = 0;
+		if  (haystack[i] == needle[i])
 		{
-			if (*needle == *haystack)
-			{
-				return (haystack);
-			}
-			needle++;
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
 		}
 		haystack++;
 	}
