1cc34fb5f23dac847e745c29bc724a4f231795b5
afolabi <maverickoluwatomisin@gmail.com>
2022-09-26 12:06:26 +0100

    Debug

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,7 +8,7 @@
  */
 char *_strchr(char *s, char c)
 {
-	int x = 0;
+	unsigned int x = 0;
 
 	while (*s != '\0')
 	{
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets length of a prefix substring
+ * @s:  String
+ * @accept: substring
+ * Return: Int val of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == *c)
+		{
+			x++;
+			c++;
+		}
+		s++;
+	}
+	return (x);
+}
