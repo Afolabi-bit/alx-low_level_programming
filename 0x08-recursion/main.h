909e516d7391b3830d8b9db519fd5bf4a7cbca47
afolabi <maverickoluwatomisin@gmail.com>
2022-09-28 16:52:14 +0100

    palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,5 @@
 #include <unistd.h>
 
-/*
- * int _prime(int n, int c)
- */
 int sq(int num, int root);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -12,3 +9,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _putchar(char c);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
