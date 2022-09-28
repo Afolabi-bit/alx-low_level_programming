6fab2f0e544f776f4763c0bbc02292fa1c82f304
afolabi <maverickoluwatomisin@gmail.com>
2022-09-28 12:53:17 +0100

    Integer to a power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Find x to the pow of y
+ * @x: integer
+ * @y: pow
+ * Return: x to pow of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
