560ca09b82b6f699594f7a7e28240641f756b0b2
afolabi <maverickoluwatomisin@gmail.com>
2022-09-28 13:22:13 +0100

    Finds sq root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Finds the sqrt of a num
+ * @n: interger
+ * Return: Sq root
+ */
+int sq(int num, int root);
+int _sqrt_recursion(int n)
+{
+	return (sq(n, 1));
+}
+
+/**
+ * sq - finds the square of numbers and compares
+ * @num: number
+ * @root: root
+ * Return: square root of num or -1
+ */
+int sq(int num, int root)
+{
+	if (root * root == num)
+	{
+		return (root);
+	}
+	else if (root * root < num)
+	{
+		return (sq(num, root + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
