a310439a0ec79d90ed110c0d33fefa5376d3773f
afolabi <maverickoluwatomisin@gmail.com>
2022-10-03 16:59:19 +0100

    Concatenates strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,32 +9,27 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *p;
-	int i, j, len1, len2, size;
+	int i, j = 0, len = 0;
 
 	if (s1 == NULL)
-		*s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		*s2 = '\0';
+		s2 = "";
 
-	for (i = 0; s1[i]; i++)
-		len1++;
-	for (i = 0; s2[i]; i++)
-		len2++;
+	for (i = 0; s1[i] || s2[i]; i++)
+		len++;
 
-	size = len1 + len2;
-	p = malloc(sizeof(char) * (size + 1));
+	p = malloc(sizeof(char) * len);
 
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < len1; i++)
-		p[i] = s1[i];
+	for (i = 0; s1[i]; i++)
+		p[j++] = s1[i];
 
-	j = 0;
-	for (i = len1; i < size; i++)
+	for (i = 0; s2[i]; i++)
 	{
-		p[i] = s2[j];
-		j++;
+		p[j++] = s2[i];
 	}
 	return (p);
 }
