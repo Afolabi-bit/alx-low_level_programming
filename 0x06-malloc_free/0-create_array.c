0b817b293469d6b665d7d3a641e9314371f5d2b0
afolabi <maverickoluwatomisin@gmail.com>
2022-10-03 16:00:41 +0100

    create array oof a character

---
+++ 0x06-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: character
+ * Return: Pointer to array or Null
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p = malloc(size * sizeof(char));
+	int i;
+
+	if (p == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		*(p + i) = c;
+		i++;
+	}
+	return (p);
+}
