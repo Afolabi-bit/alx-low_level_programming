1e4e76de8813f83b815df766b51531b93c67f282
afolabi <maverickoluwatomisin@gmail.com>
2022-10-03 16:27:53 +0100

    string duplicate

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,13 +10,19 @@ char *_strdup(char *str)
 	char *p;
 	int i, strlen;
 
+	if (str == NULL)
+		return (NULL);
+
 	for (i = 0; str[i]; i++)
 		strlen++;
-	p = malloc(sizeof(char) * strlen);
-	if (str == NULL || p == NULL)
+	p = malloc(sizeof(char) * (strlen + 1));
+
+	if (p == NULL)
 		return (NULL);
+
 	for (i = 0; str[i]; i++)
 		p[i] = str[i];
+
 	p[strlen] = '\0';
 
 	return (p);
