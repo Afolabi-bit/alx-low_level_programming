e36ff9050f81f0bbac0334f466d22c7a6ab342ad
afolabi <maverickoluwatomisin@gmail.com>
2022-10-10 12:34:17 +0100

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+# include <unistd.h>
+/**
+ * struct dog - Info about a dog
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner's name
+ *
+ * Description: Stores a dog's basic info
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
