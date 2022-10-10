ead216d5cac6ea3c4656730abd7a29b95698dab3
afolabi <maverickoluwatomisin@gmail.com>
2022-10-10 14:21:22 +0100

    Free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dogs - Frees dogs from memory
+ * @d: Dog struct
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+	}
+}
