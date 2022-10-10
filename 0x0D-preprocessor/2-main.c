9acd4fcad841cb8a43c54654e48a72e0afcc6762
afolabi <maverickoluwatomisin@gmail.com>
2022-10-10 15:47:16 +0100

    Prints the filename

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define NAME __FILE__
+
+/**
+ * main -prints the name og the program
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", NAME);
+
+	return (0);
+}
