dea53d374ca3c90d6206b44f03425a898ced872f
afolabi <maverickoluwatomisin@gmail.com>
2022-10-12 13:32:41 +0100

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int s = size, i;
 
-	if (action != NULL)
+	if (action != NULL && array != NULL)
 	{
 		for (i = 0; i < s; i++)
 		{
