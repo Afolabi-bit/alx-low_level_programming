5fbf3864751cf59dc5aff7106afee0907528a390
afolabi <maverickoluwatomisin@gmail.com>
2022-10-12 14:04:02 +0100

    Search array for an int fitting a conditon

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, c;
 
-	if (cmp == NULL || size <= 0)
+	if (cmp == NULL)
+		return NULL;
+	if (size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
