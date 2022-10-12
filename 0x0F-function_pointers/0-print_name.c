e6cda38799e6135650f2ef2fb276263c7ff80582
afolabi <maverickoluwatomisin@gmail.com>
2022-10-12 13:13:00 +0100

    Print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_name - prints name passed it
+ * @name: name passed
+ * @f: pointer to a function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
