2120dd08d0d2393e34590c8111f27f90b339584b
afolabi <maverickoluwatomisin@gmail.com>
2022-10-21 16:40:34 +0100

    add to end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,7 +2,7 @@
 
 /**
  * add_node_end - adds a new node at the end of a list_t list
- * 
+ *
  * @head: adr of list
  *
  * @str: string data of new node
@@ -40,7 +40,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	temp = *head;
-	while(temp->next)
+	while (temp->next)
 		temp = temp->next;
 	temp->next = new;
 	return (new);
