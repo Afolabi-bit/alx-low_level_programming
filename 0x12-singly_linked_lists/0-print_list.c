d8ac8c2eba62ecc696788f60650a3f1d05014130
afolabi <maverickoluwatomisin@gmail.com>
2022-10-21 15:11:23 +0100

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list
+ *
+ * @h: address of the node head
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num = 0;
+	char *s;
+	int n;
+
+	while (h)
+	{
+		n = h->len;
+		s = h->str;
+
+		num++;
+		if (h->str)
+		{
+			_putchar('[');
+			_putchar(n + '0');
+			_putchar(']');
+			_putchar(' ');
+
+			while (*s)
+			{
+				_putchar(*s);
+				s++;
+			}
+		}
+		else
+		{
+			_putchar('[');
+			_putchar(0 + '0');
+			_putchar(']');
+			_putchar(' ');
+			_putchar('(');
+			_putchar('n');
+			_putchar('i');
+			_putchar('l');
+			_putchar(')');
+		}
+		_putchar('\n');
+
+		h = h->next;
+	}
+	return (num);
+}
