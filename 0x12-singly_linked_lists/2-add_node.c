e57ccbd37c89fc06b2398d680fb312f50ea7331e
afolabi <maverickoluwatomisin@gmail.com>
2022-10-21 16:00:56 +0100

    add to head

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list
+ *
+ * @head: pointer to the adr of the first node
+ *
+ * @str: string to be added to new node
+ *
+ * Return: address of new element,
+ * NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	while (*str)
+		len++;
+	new = malloc(sizeof(list_t));
+
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define _MAIN_
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
