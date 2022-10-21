1ce4a34fa67bdf0e4808d9dd81797d6c72fd0cf0
afolabi <maverickoluwatomisin@gmail.com>
2022-10-21 15:27:17 +0100

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - Returns the num of elements
+ * in a linked list
+ *
+ * @h: pointer to list
+ *
+ * Return: length of list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+	
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
