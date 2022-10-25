378cd5f3fdd905fb1b215ac284a394afb2d05974
afolabi <maverickoluwatomisin@gmail.com>
2022-10-25 22:55:36 +0100

    linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (temp == NULL)
 		return (NULL);
 
-	if (!(*head))
+	if (!(*head) && idx != 0)
 		return (NULL);
 	temp1 = *head;
 
