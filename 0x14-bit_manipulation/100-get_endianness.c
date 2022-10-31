95c4370c1e8af71d461c4d17ce2c2ec6466b005c
afolabi <maverickoluwatomisin@gmail.com>
2022-10-31 14:49:09 +0100

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianess of machine
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int x = 0x72562233;
+	char *c = (char*) &x;
+
+	if (*c == 0x33)
+		return (1);
+	else
+		return (0);
+}
