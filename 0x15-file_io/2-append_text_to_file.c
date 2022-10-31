860397ee2838df59659a46df88514ce0551f746c
afolabi <maverickoluwatomisin@gmail.com>
2022-11-01 00:56:59 +0100

    file i/o

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
-		return(-1);
+		return (-1)
 	close(fd);
 
 	return (1);
